e8bdad9353e046693e70e35eae7590268a58ad84
ygangmin <ygangmin@gmail.com>
2023-07-26 17:33:47 +0900

    Initial commit

---
+++ src/main/java/com/example/fishrecog/FishrecogApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishrecog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishrecogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishrecogApplication.class, args);
+    }
+
+}
